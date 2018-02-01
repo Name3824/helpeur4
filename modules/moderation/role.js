cd945255b85778abe58a28c4c7e8e585d7c3e0c7
Chtigaming <35092386+Chtigaming@users.noreply.github.com>
2018-02-01 18:12:09 +0100

    Update role.js

--- modules/moderation/role.js
+++ modules/moderation/role.js
@@ -5,10 +5,9 @@ function role(msg,prefix,client){
         var searchrole = msg.content.slice(msg.content.indexOf(msg.content.split(" ")[1]));						
             var roleauthor = msg.guild.member(msg.author);	
             if(searchrole.includes("Homme") || searchrole.includes("Femme") || searchrole.includes("celibataire") || searchrole.includes("en couple")){
-        var homme = msg.guild.roles.find("name", "Homme");
-        var femme = msg.guild.roles.find("name", "Femme")
-        var celib = msg.guild.roles.find("name", "celibataire")
-        var couple = msg.guild.roles.find("name", "en couple")
+        var homme = msg.guild.roles.find("name", "Joueur Build");
+        var femme = msg.guild.roles.find("name", "Joueur Faction")
+        var couple = msg.guild.roles.find("name", "Joueur DayZ")
         
         roleauthor.addRole(msg.guild.roles.find("name", searchrole)).then(msg.channel.send("Rôle ajouter avec succée `"+searchrole+"` :smile:"));
             
